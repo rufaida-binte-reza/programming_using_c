6bc5c630b1a26af8e12c2a339b43c141aa9570d9
rufaida-binte-reza <rufaida.reza.bubt@gmail.com>
2023-07-16 02:43:42 +0600

    1. Introduction project added.
    2. User input project added.

---
+++ Introduction/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    printf("Name:\t\tRufaida Binte Reza\n");
+    printf("Father's Name:\tRazaul Karim\n");
+    printf("Mother's Name:\tArfatun Nahar\n");
+    printf("DOB:\t\t19/11/2002\n");
+    printf("Nationality:\tBangladeshi\n");
+
+    return 0;
+}
